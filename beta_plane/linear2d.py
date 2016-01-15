c2c03a38f29753c059b8fb9087664470664a849e
James Penn <james@jamespenn.co.uk>
2016-01-15 17:46:56 +0000

    Including changes to linear2d.py as well...

--- beta_plane/linear2d.py
+++ beta_plane/linear2d.py
@@ -20,7 +20,7 @@ f = f0 + βy
 import numpy as np
 
 class LinearShallowWater(object):
-    def __init__(self, nx, ny, Lx=1.0e7, Ly=1.0e7, f0=0, beta=2.0e-11, H=100.0, g=0.05, nu=1.0e-5, dt=1000.0, bcond='periodicx'):
+    def __init__(self, nx, ny, Lx=1.0e7, Ly=1.0e7, f0=0, beta=2.0e-11, H=100.0, g=0.05, nu=1.0e-5, r=1.0e-5, dt=1000.0, bcond='periodicx'):
         super(LinearShallowWater, self).__init__()
         self.nx = nx
         self.ny = ny
@@ -30,7 +30,6 @@ class LinearShallowWater(object):
         self.beta = beta
         self.H = H
         self.g = g
-        self.nu = nu
 
         # Arakawa-C grid
         # +-- v --+   
@@ -57,11 +56,21 @@ class LinearShallowWater(object):
 
         self.bcond = bcond
 
+        # dissipation and friction
+        self.nu = nu
+        self.r = r
+        self.sponge_ny = ny//7
+        self.sponge = np.exp(-np.linspace(0, 5, self.sponge_ny))
+        #self.sponge = np.linspace(1.0, 0.0, self.sponge_ny)
+        #print(self.sponge)
+
         # timestepping
         self._pdstate, self._ppdstate = 0.0, 0.0
         self.dt = dt
         self.tc = 0  # number of timesteps taken
         self.t = 0.0
+
+        self._forcings = []
 
     # define u, v and h properties to return state without the boundaries
     @property
@@ -86,6 +95,32 @@ class LinearShallowWater(object):
         self.u[:] = u
         self.v[:] = v
         self.h[:] = h
+
+
+    def add_forcing(self, fn):
+        """Add a forcing term to the model.  Typically used as a decorator:
+
+            sw = LinearShallowWater(nx, ny)
+
+            @sw.add_forcing
+            def dissipate(swmodel):
+                dstate = np.zeros_like(swmodel.state)
+                dstate[:] = -swmodel.state*0.001
+                return dstate
+
+        Forcing functions should take a single argument for the model object itself,
+        and return a state delta the same shape as state.
+        """
+        self._forcings.append(fn)
+        return fn
+
+    def damping(self, var):
+        # sponges are active at the top and bottom of the domain by applying Rayleigh friction
+        # with exponential decay towards the centre of the domain
+        var_sponge = np.zeros_like(var)
+        var_sponge[:, :self.sponge_ny] = self.sponge[np.newaxis, :]
+        var_sponge[:, -self.sponge_ny:] = self.sponge[::-1][np.newaxis, :]
+        return self.r*var_sponge*var
 
 
     # Define finite-difference methods on the grid
@@ -118,7 +153,7 @@ class LinearShallowWater(object):
          
         The derivative is returned at the same x points as the
         x points of the input array, with dimension (nx-2, ny)."""
-        return (phi[:-2, :] - phi[1:-1, :] + phi[2:, :]) / self.dx**2
+        return (phi[:-2, :] - 2*phi[1:-1, :] + phi[2:, :]) / self.dx**2
 
     def diff2y(self, phi):
         """Calculate ∂2/∂y2[phi] over a single grid square.
@@ -127,7 +162,7 @@ class LinearShallowWater(object):
          
         The derivative is returned at the same y points as the
         y points of the input array, with dimension (nx, ny-2)."""
-        return (phi[:, :-2] - phi[:, 1:-1] + phi[:, 2:]) / self.dy**2
+        return (phi[:, :-2] - 2*phi[:, 1:-1] + phi[:, 2:]) / self.dy**2
 
     def centre_average(self, phi):
         """Returns the four-point average at the centres between grid points.
@@ -172,8 +207,8 @@ class LinearShallowWater(object):
 
         # top and bottom boundaries: zero deriv and damping
         for field in fields:
-            field[:, 0] = field[:, 1]*0.9
-            field[:, -1] = field[:, -2]*0.9
+            field[:, 0] = field[:, 1]
+            field[:, -1] = field[:, -2]
 
         for field in fields:
             # fix corners to be average of neighbours
@@ -181,6 +216,12 @@ class LinearShallowWater(object):
             field[-1, 0] = 0.5*(field[-2, 0] + field[-1, 1])
             field[0, -1] = 0.5*(field[1, -1] + field[0, -2])
             field[-1, -1] = 0.5*(field[-1, -2] + field[-2, -1])
+
+    def uvath(self):
+        """Calculate the value of u at h points (cell centres)."""
+        ubar = self.x_average(self.u)  # (nx, ny)
+        vbar = self.y_average(self.v)  # (nx, ny)
+        return ubar, vbar
 
     def uvatuv(self):
         """Calculate the value of u at v and v at u."""
@@ -194,19 +235,24 @@ class LinearShallowWater(object):
         
         self._apply_boundary_conditions()
         uu, vv = self.uvatuv()
-        
+
         # the height equation
-        h_rhs = -H*self.divergence() + nu*self.del2(self._h) # - self.damping(self._h)[1:-1, 1:-1]
+        h_rhs = -H*self.divergence() + nu*self.del2(self._h) - self.damping(self.h)
 
         # the u equation
         dhdx = self.diffx(self._h)[:, 1:-1]
-        u_rhs = (f0 + beta*self.uy)*vv - g*dhdx + nu*self.del2(self._u) # - self.damping(self._u)[1:-1, 1:-1]
+        u_rhs = (f0 + beta*self.uy)*vv - g*dhdx + nu*self.del2(self._u) - self.damping(self.u)
         
         # the v equation
         dhdy  = self.diffy(self._h)[1:-1, :]
-        v_rhs = -(f0 + beta*self.vy)*uu - g*dhdy + nu*self.del2(self._v) # - self.damping(self._v)[1:-1, 1:-1]
+        v_rhs = -(f0 + beta*self.vy)*uu - g*dhdy + nu*self.del2(self._v) - self.damping(self.v)
 
-        return np.array([u_rhs, v_rhs, h_rhs])
+        dstate = np.array([u_rhs, v_rhs, h_rhs])
+        
+        for fn in self._forcings:
+            dstate += fn(self)
+
+        return dstate
 
     def step(self):
         dt, tc = self.dt, self.tc
@@ -241,53 +287,55 @@ class LinearShallowWater(object):
         self.tc += 1
 
 
-nx = 128
-ny = 129
-beta=2.0e-11
-Lx = 1.0e7
-Ly = 1.0e7
+if __name__ == '__main__':
+        
+    nx = 128
+    ny = 129
+    beta=2.0e-11
+    Lx = 1.0e7
+    Ly = 1.0e7
 
-ocean = LinearShallowWater(nx, ny, Lx, Ly, beta=beta, f0=0.0, g=0.1, H=100.0, dt=3000, bcond='wallsx')
-#ocean.h[10:20, 60:80] = 1.0
-#ocean.h[-20:-10] = 1.0
-d = 25
-ocean.h[10:10+2*d, ny//2-d:ny//2+d] = (np.sin(np.linspace(0, np.pi, 2*d))**2)[np.newaxis, :] * (np.sin(np.linspace(0, np.pi, 2*d))**2)[:, np.newaxis]
-#ocean.h[100:100+2*d, ny//2-d:ny//2+d] = (np.sin(np.linspace(0, np.pi, 2*d))**2)[np.newaxis, :] * (np.sin(np.linspace(0, np.pi, 2*d))**2)[:, np.newaxis]
-import matplotlib.pyplot as plt
+    ocean = LinearShallowWater(nx, ny, Lx, Ly, beta=beta, f0=0.0, g=0.1, H=100.0, dt=3000, nu=1000.0, bcond='wallsx')
+    #ocean.h[10:20, 60:80] = 1.0
+    #ocean.h[-20:-10] = 1.0
+    d = 25
+    ocean.h[10:10+2*d, ny//2-d:ny//2+d] = (np.sin(np.linspace(0, np.pi, 2*d))**2)[np.newaxis, :] * (np.sin(np.linspace(0, np.pi, 2*d))**2)[:, np.newaxis]
+    #ocean.h[100:100+2*d, ny//2-d:ny//2+d] = (np.sin(np.linspace(0, np.pi, 2*d))**2)[np.newaxis, :] * (np.sin(np.linspace(0, np.pi, 2*d))**2)[:, np.newaxis]
+    import matplotlib.pyplot as plt
 
 
-atmos = LinearShallowWater(nx, ny, Lx, Ly, beta=beta, f0=0.0, g=3.0, H=10.0, dt=1000, bcond='periodicx')
-plt.ion()
+    atmos = LinearShallowWater(nx, ny, Lx, Ly, beta=beta, f0=0.0, g=3.0, H=10.0, dt=1000, bcond='periodicx')
+    plt.ion()
 
-num_levels = 24
-colorlevels = np.concatenate([np.linspace(-1, -.05, num_levels//2), np.linspace(.05, 1, num_levels//2)])
+    num_levels = 24
+    colorlevels = np.concatenate([np.linspace(-1, -.05, num_levels//2), np.linspace(.05, 1, num_levels//2)])
 
-ts = []
-es = []
-plt.show()
-for i in range(10000):
-    ocean.step()
-    if i % 10 == 0:
-        plt.figure(1)
-        plt.clf()
-        #plt.plot(ocean.h[:,0])
-        #plt.plot(ocean.h[:,64])
-        #plt.ylim(-1,1)
-        plt.contourf(ocean.h.T, cmap=plt.cm.RdBu, levels=colorlevels)
+    ts = []
+    es = []
+    plt.show()
+    for i in range(10000):
+        ocean.step()
+        if i % 10 == 0:
+            plt.figure(1)
+            plt.clf()
+            #plt.plot(ocean.h[:,0])
+            #plt.plot(ocean.h[:,64])
+            #plt.ylim(-1,1)
+            plt.contourf(ocean.h.T, cmap=plt.cm.RdBu, levels=colorlevels)
 
-        plt.figure(2)
-        plt.clf()
-        plt.plot(ocean.h[:,0])
-        plt.plot(ocean.h[:,48])
-        plt.plot(ocean.h[:,64])
-        plt.ylim(-1,1)
+            plt.figure(2)
+            plt.clf()
+            plt.plot(ocean.h[:,0])
+            plt.plot(ocean.h[:,48])
+            plt.plot(ocean.h[:,64])
+            plt.ylim(-1,1)
 
-        plt.figure(3)
-        plt.clf()
-        energy = np.sum(ocean.g*ocean.h) + np.sum(ocean.u**2) + np.sum(ocean.v**2)
-        ts.append(ocean.t)
-        es.append(energy)
-        plt.plot(ts, es)
+            plt.figure(3)
+            plt.clf()
+            energy = np.sum(ocean.g*ocean.h) + np.sum(ocean.u**2) + np.sum(ocean.v**2)
+            ts.append(ocean.t)
+            es.append(energy)
+            plt.plot(ts, es)
 
-        plt.pause(0.01)
-        plt.draw()
+            plt.pause(0.01)
+            plt.draw()
