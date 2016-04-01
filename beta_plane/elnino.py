b4a359ebc29d318390c4f965f03fd926a696418e
James Penn <james@jamespenn.co.uk>
2016-04-01 16:57:16 +0100

    Updated elnino.py to run with new linear models.

--- beta_plane/elnino.py
+++ beta_plane/elnino.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from linear2d import LinearShallowWater
+from linear import PeriodicLinearShallowWater, WalledLinearShallowWater
 
 np.set_printoptions(precision=2, suppress=True)  # 2 dp and hide floating point error
 
@@ -38,26 +38,38 @@ ocean_dt = 2500.0
 atmos_dt = ocean_dt / 10
 
 
-atmos = LinearShallowWater(nx, ny, Lx, Ly, beta=beta, f0=f0, g=g_atmos, H=H_atmos,  dt=atmos_dt, nu=nu_atmos, r=1e-5, bcond='periodicx')
-ocean = LinearShallowWater(nx, ny, Lx, Ly, beta=beta, f0=f0, g=g_ocean, H=H_ocean, dt=ocean_dt, nu=nu_ocean, r=1e-6, bcond='wallsx')
+atmos = PeriodicLinearShallowWater(nx, ny, Lx, Ly,
+            beta=beta, f0=f0,
+            g=g_atmos, H=H_atmos,
+            dt=atmos_dt, nu=nu_atmos, r=1e-5)
+ocean = WalledLinearShallowWater(nx, ny, Lx, Ly,
+            beta=beta, f0=f0,
+            g=g_ocean, H=H_ocean,
+            dt=ocean_dt, nu=nu_ocean, r=1e-6)
 
 @atmos.add_forcing
 def heating(a):
-    global alpha
-    return np.array([[0],[0],alpha*ocean.h])
+    global alpha, ocean
+    dstate = np.zeros_like(a.state)
+    dstate[2] = alpha*ocean.h
+    return dstate
 
 @ocean.add_forcing
 def wind_stress(o):
-    global gamma
-    return np.array([-gamma*atmos.u, [0], [0]])
+    global gamma, atmos
+    dstate = np.zeros_like(o.state)
+    dstate[0] = -gamma*atmos.u
+    return dstate
 
 @atmos.add_forcing
 def trade_winds(a):
+    dstate = np.zeros_like(a.state)
     gust = np.zeros_like(a.u)
     if a.tc % 10000 == 0:
         print('gust!')
         gust[nx//2-d:nx//2+d, ny//2-d:ny//2+d] = -H_atmos * 0.01 / atmos_dt * (np.sin(np.linspace(0, np.pi, 2*d))**2)[np.newaxis, :] * (np.sin(np.linspace(0, np.pi, 2*d))**2)[:, np.newaxis]
-    return np.array([gust,[0],[0]])
+    dstate[0] = gust
+    return dstate
 
 # Initial Condition
 d = 25
@@ -66,8 +78,9 @@ d = 25
 #atmos.h[nx//2-d:nx//2+d, ny//2-d:ny//2+d] = H_atmos * 0.01 * (np.sin(np.linspace(0, np.pi, 2*d))**2)[np.newaxis, :] * (np.sin(np.linspace(0, np.pi, 2*d))**2)[:, np.newaxis]
 #atmos.u[nx//2-d:nx//2+d, ny//2-d:ny//2+d] = H_atmos * 0.01 * (np.sin(np.linspace(0, np.pi, 2*d))**2)[np.newaxis, :] * (np.sin(np.linspace(0, np.pi, 2*d))**2)[:, np.newaxis]
 #ocean.h[:] = np.random.random((nx, ny)) -0.5
-plt.ion()
+
 plt.figure(figsize=(12, 12))
+plt.ion()
 
 num_levels = 24
 colorlevels = np.concatenate([np.linspace(-1, -.05, num_levels//2), np.linspace(.05, 1, num_levels//2)])
@@ -108,10 +121,10 @@ for i in range(1000000):
 
             u, v  = atmos.uvath()/absmax(atmos.u)
             vel = np.sqrt(u**2 + v**2)
-                    
+
             print('Ocean Velocity: %.3f' % absmax(velmag(ocean)))
             print('Atmos Velocity: %.3f\n' % absmax(velmag(atmos)))
-        
+
             plt.clf()
             plt.subplot(221)
             plt.contourf(hx, hy, ocean.h.T, cmap=plt.cm.RdBu, levels=colorlevels*absmax(ocean.h))
@@ -120,8 +133,8 @@ for i in range(1000000):
             plt.subplot(222)
             plt.contourf(hx, hy, atmos.h.T, cmap=plt.cm.RdBu, levels=colorlevels*H_atmos*0.05)#absmax(atmos.h))
             plt.colorbar()
-            plt.quiver(hx[arrow_spacing], hy[arrow_spacing], 
-                np.ma.masked_where(vel.T < 0.1, u.T)[arrow_spacing], 
+            plt.quiver(hx[arrow_spacing], hy[arrow_spacing],
+                np.ma.masked_where(vel.T < 0.1, u.T)[arrow_spacing],
                 np.ma.masked_where(vel.T < 0.1, v.T)[arrow_spacing], pivot='mid', scale=15, width=0.005)
 
             # plt.subplot(223)
@@ -133,7 +146,7 @@ for i in range(1000000):
             plt.colorbar()
 
             plt.subplot(224)
-            
+
             # if len(equator_zonal_winds) % 2 == 1:
             #     power = np.log(np.abs(np.fft.fft2(np.array(equator_zonal_winds))**2))
             # else:
