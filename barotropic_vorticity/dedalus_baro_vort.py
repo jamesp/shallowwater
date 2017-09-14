0337052f62171896a9cc9cd9765e2515f4315c9d
James Penn <james@jamespenn.co.uk>
2017-09-14 12:16:10 +0100

    Dedalus barotropic vorticity now works.

--- barotropic_vorticity/dedalus_baro_vort.py
+++ barotropic_vorticity/dedalus_baro_vort.py
@@ -33,7 +33,6 @@ numerically.
 import logging
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 from dedalus import public as de
 from dedalus.extras import flow_tools
@@ -44,19 +43,23 @@ for h in root.handlers:
 
 logger = logging.getLogger(__name__)
 
-N = 96
+PLOTTING = False
+q = 1
+N = 96*2**q
 Lx, Ly = (1., 1.)
 nx, ny = (N, N)
-beta = 8.0
+beta = 0.0
 U = 0.0
-dt = 1e-3
+D = 1e-16
+dt = 1e-6
+
 
 # setup the domain
 x_basis = de.Fourier('x', nx, interval=(0, Lx), dealias=3/2)
 y_basis = de.Fourier('y', ny, interval=(0, Ly), dealias=3/2)
 domain = de.Domain([x_basis, y_basis], grid_dtype=np.float64)
 
-problem = de.IVP(domain, variables=['psi', 'zeta'])
+problem = de.IVP(domain, variables=['psi'])
 
 
 # solve the problem from the equations
@@ -64,31 +67,32 @@ problem = de.IVP(domain, variables=['psi', 'zeta'])
 # ∂/∂t[∆ψ] + β ∂/∂x[ψ] = -J(ζ, ψ)
 
 # Everytime you ask for one of the expression on the left, you will get the expression on the right.
+problem.substitutions['L(a)']         = "  (d(a,x=2) + d(a,y=2)) "
+problem.substitutions['J(a,b)'] = "  (dx(a)*dy(b) - dy(a)*dx(b)) "
+
 problem.substitutions['u']    = " -dy(psi) "
 problem.substitutions['v']    = "  dx(psi) "
-
-problem.substitutions['L(thing_1)']         = "  (d(thing_1,x=2) + d(thing_1,y=2)) "
-problem.substitutions['J(thing_1,thing_2)'] = "  (dx(thing_1)*dy(thing_2) - dy(thing_1)*dx(thing_2)) "
+problem.substitutions['zeta'] = " L(psi) "
 
 # You can combine things if you want
-problem.substitutions['HD(phi, n)']         = "  -D*(d(phi, x=n) + d(phi, y=n)) "
+problem.substitutions['HD(a, n)']         = "  -D*(d(a, x=n) + d(a, y=n)) "
 
 problem.parameters['beta'] = beta
 problem.parameters['U']    = U
-problem.parameters['D']   = 1e-10 # hyperdiffusion coefficient
+problem.parameters['D']   = D # hyperdiffusion coefficient
 
-problem.add_equation("dt(zeta) + beta*v - HD(zeta, 8) = J(zeta, psi) ")
-problem.add_equation("psi = 0",                                     condition="(nx == 0) and (ny == 0)")
-problem.add_equation("zeta = L(psi)", condition="(nx != 0) or  (ny != 0)")
+problem.add_equation("dt(zeta) + beta*v - HD(zeta, 8) = J(zeta, psi) ", condition="(nx != 0) or  (ny != 0)")
+problem.add_equation("psi = 0", condition="(nx == 0) and (ny == 0)")
+#problem.add_equation("zeta = L(psi)", condition="(nx != 0) or  (ny != 0)")
 
 
 solver = problem.build_solver(de.timesteppers.SBDF3)
-solver.stop_sim_time  = np.inf
+solver.stop_sim_time  = .1
 solver.stop_wall_time = np.inf
-solver.stop_iteration = 1000
+solver.stop_iteration = np.inf
 
 # vorticity & velocity are no longer states of the system. They are true diagnostic variables.
-# But you still might want to set initial condisitons based on vorticity (for example).
+# But you still might want to set initial condiitons based on vorticity (for example).
 # To do this you'll have to solve for the streamfunction.
 
 # This will solve for an inital psi, given a vorticity field.
@@ -101,6 +105,7 @@ noise = rand.standard_normal(gshape)[slices]
 
 init_vorticity = domain.new_field()
 init_vorticity.set_scales(1)
+
 
 k = domain.bases[0].wavenumbers[:, np.newaxis]
 l = domain.bases[1].wavenumbers[np.newaxis, :]
@@ -118,11 +123,11 @@ init_vorticity['c'] = (-ksq*piit)[cslices]
 
 # x,y = domain.grids(scales=1)
 
-# init_vorticity['g'] =  (0.5)*noise +  3*np.exp( - 80* ( (x-Lx/2)**2 + (y-Ly/4)**2 ) )
+#init_vorticity['g'] =  (0.5)*noise #+  3*np.exp( - 80* ( (x-Lx/2)**2 + (y-Ly/4)**2 ) )
 
 init.parameters['init_vorticity'] = init_vorticity
 
-init.add_equation(" d(init_psi,x=2) + d(init_psi,y=2) = init_vorticity ", condition="(nx != 0) or  (ny != 0)")
+init.add_equation(" d(init_psi,x=2) + d(init_psi,y=2) = init_vorticity", condition="(nx != 0) or  (ny != 0)")
 init.add_equation(" init_psi = 0",                                        condition="(nx == 0) and (ny == 0)")
 
 init_solver = init.build_solver()
@@ -131,36 +136,57 @@ init_solver.solve()
 psi = solver.state['psi']
 psi['g'] = init_solver.state['init_psi']['g']
 
+# plt.imshow(init_vorticity['g'])
+# plt.show()
+
+
+# plt.imshow(psi['g'])
+# plt.show()
+
 # Now you are ready to go.
 # Anytime you ask for zeta, u, or v they will be non-zero because psy is non-zero.
 
-cfl = flow_tools.CFL(solver, initial_dt=dt, cadence=10, safety=2,
+cfl = flow_tools.CFL(solver, initial_dt=dt, cadence=10, safety=.5,
                      max_change=1.5, min_change=0.5)
 cfl.add_velocities(('u','v'))
 
-dout = solver.evaluator.add_dictionary_handler(iter=1)
-dout.add_system(solver.state)
-dout.add_task('zeta', scales=1, name='zeta')
-dout.add_task('u', scales=1, name='u')
-dout.add_task('v', scales=1, name='v')
 
-plt.ion()
-fig, axis = plt.subplots(figsize=(10,5))
+dout = solver.evaluator.add_dictionary_handler(iter=10)
+fout = solver.evaluator.add_file_handler('analysis_tasks', sim_dt=1e-3)
 
-im = axis.imshow(init_vorticity['g'].T, cmap=plt.cm.YlGnBu)
-plt.pause(1)
+outputs = [fout]
+if PLOTTING:
+    outputs.append(dout)
+
+for output in outputs:
+    output.add_system(solver.state)
+    output.add_task('zeta', scales=1, name='zeta')
+    output.add_task('u', scales=1, name='u')
+    output.add_task('v', scales=1, name='v')
+
+
+if PLOTTING:
+    import matplotlib.pyplot as plt
+    plt.ion()
+    fig, axis = plt.subplots(figsize=(10,5))
+
+    im = axis.imshow(init_vorticity['g'].T, cmap=plt.cm.YlGnBu)
+    plt.pause(1)
 
 logger.info('Starting loop')
 while solver.ok:
-    dt = cfl.compute_dt()   # this is returning inf after the first timestep
+    dt = cfl.compute_dt()
     # print(dt)
     solver.step(dt)
-    if solver.iteration % 1 == 0:
-        zeta = dout.fields['zeta']['g']
-        im.set_data(zeta.T)
-        maxzeta = np.max(np.abs(zeta))
-        im.set_clim(-maxzeta, maxzeta)
-        plt.pause(0.01)
+    if solver.iteration % 10 == 0:
+        if PLOTTING:
+            zeta = dout.fields['zeta']['g']
+            psi = solver.state['psi']['g']
+            im.set_data(zeta.T)
+            im.set_clim(np.min(zeta), np.max(zeta))
+            #maxzeta = np.max(np.abs(zeta))
+            #im.set_clim(-maxzeta, maxzeta)
+            plt.pause(0.01)
         logger.info('Iteration: %i, Time: %e, dt: %e' %(solver.iteration, solver.sim_time, dt))
 
 # Print statistics
